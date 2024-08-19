a10922537880fc1da07049bcb008df3c7862ea90
TheMaskedZero <101093397+TheMaskedZero@users.noreply.github.com>
2024-08-19 15:10:27 +0200

    Participant menu ui

---
+++ Assets/Scripts/Participant Data.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticipantData : MonoBehaviour
+{
+    public static int participantNumber;
+    public static int participantAge;
+    
+    public static string participantEyeCorrection;
+    public static string participantName;
+    public static string participantGender;
+    public static string participantNearFarSight;
+    public static string participantEyeColour;
+    public static string participantCountryBirth;
+    public static string participantCurrentResidency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
